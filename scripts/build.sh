a4c01988f9e9ccf4d2a89fbcb773bac2f3d9a085
YetAnotherClown <yetanotherclown@proton.me>
2025-02-03 17:18:47 -0500

    Exclude tests in build

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,4 +10,7 @@ fi
 rojo sourcemap default.project.json -o sourcemap.json
 
 darklua process --config .darklua.json src/ dist/src
+rm -rf dist/src/__tests__
+rm -f dist/src/jest.config.luau
+
 rojo build build.project.json -o Planck.rbxm

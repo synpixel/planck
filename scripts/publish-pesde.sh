9f528432bbc8b3d7d5e60b539baf4880f65854a9
synpixel <syntaxwastaken2@gmail.com>
2025-02-15 15:37:11 +0100

    fix: clone lock file before publishing

--- scripts/publish-pesde.sh
+++ scripts/publish-pesde.sh
@@ -10,6 +10,7 @@ darklua process --config .darklua.json src/ dist/src
 cp README.md dist/README.md
 cp LICENSE.md dist/LICENSE.md
 cp pesde.toml dist/pesde.toml
+cp pesde.lock dist/pesde.lock
 
 cd ./dist
 
